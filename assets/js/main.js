86f86bc1c345265a24e0d91346faed2869c018c4
Jarrett Malifa <jarrettmalifa@Jarretts-iMac.local>
2014-05-04 23:14:02 +1200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        verticalCentered: false,
+        resize : true,
+        scrollingSpeed: 1000,
+        easing: 'easeOutQuart',
+        anchors: ['firstPage', 'secondPage', 'thirdPage'],
+    	menu: '#myMenu',
+        
+        navigation: true,
+
+
+
+
+    	'onLeave': function(index, direction){
+
+            if(index == '1' && direction =='down'){
+                $(".logo").fadeOut(1);
+            }   
+
+            if(index == '2' && direction =='down' || direction == 'up'){
+                $(".logo").fadeOut(1);
+            }
+
+            if(index == '3' && direction == 'up'){
+                $(".logo").fadeOut(1);
+            }
+                   
+        },
+
+        'afterLoad': function(anchorLink, index){
+			
+
+            if(index == '1'){
+                $(".logo").fadeIn(700);
+            }
+
+			     if(index == '2'){
+				      $(".logo").fadeIn(700);
+
+			     }
+
+          if(index == '3'){
+              $(".logo").fadeIn(700);
+          }
+
+
+		},
+
+
+    });
+});
+
+$('.logo').click(function(e) {  
+            $.fn.fullpage.moveTo('firstPage');        
+});
+
+
+// $(".").animate({top:100, opacity:"show"}, 1300);
+// $(".box1").animate({top:0, opacity:"hide"}, 0);
+
+// $('img').hide().one("load",function(){
+//     $(this).fadeIn(800);
+// }).each(function(){
+//     if(this.complete) $(this).trigger("load");
+// })
+
+
+
+
